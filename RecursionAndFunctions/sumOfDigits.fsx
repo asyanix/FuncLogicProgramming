e291bd3d59b093825c0bb7d08808b7a10e93c6ba
Asya <111695385+asyanix@users.noreply.github.com>
2025-03-19 17:14:18 +0300

    LR5: Write a recursive function to find the sum of digits of a number. Use downward recursion and tail recursion.

--- RecursionAndFunctions/sumOfDigits.fsx
+++ RecursionAndFunctions/sumOfDigits.fsx
@@ -1,12 +1,32 @@
-let sumCifr n =
-    let rec sumCifr1 n curSum =
+// Рекурсия вверх
+let sumDigitsUp n =
+    let rec sumDigitsUp1 n curSum =
         if n = 0 then curSum
         else
             let n1 = n / 10
             let cifr = n % 10
             let newSum = curSum + cifr
-            sumCifr1 n1 newSum
-    sumCifr1 n 0
+            sumDigitsUp1 n1 newSum
+    sumDigitsUp1 n 0
 
-let result = sumCifr 12345
+let result = sumDigitsUp 12345
 printfn "Сумма цифр: %d" result
+
+// Рекурсия вниз
+let rec sumDigitsDown n =
+    if n = 0 then 0
+    else (n%10) + (sumDigitsDown (n / 10))
+
+let result2 = sumDigitsDown 12345
+printfn "Сумма цифр: %d" result2
+
+// Хвостовая рекурсия 
+let rec sumDigitsTailRec (n: int, acc: int) : int =
+    if n = 0 then acc                               // Базовый случай: если n = 0, возвращаем накопитель acc
+    else sumDigitsTailRec (n / 10, acc + n % 10)    // Рекурсивный случай: убираем последнюю цифру и добавляем её в acc
+
+let sumDigits n = sumDigitsTailRec (n, 0)
+
+// Пример использования
+let result3 = sumDigits 12345 
+printfn "Сумма цифр: %d" result3
