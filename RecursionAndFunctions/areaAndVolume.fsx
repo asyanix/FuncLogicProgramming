d7558352ab32c4164ead713d6840184a6b8759bd
Asya <111695385+asyanix@users.noreply.github.com>
2025-03-19 16:55:50 +0300

    LR5: Change task 3

--- RecursionAndFunctions/areaAndVolume.fsx
+++ RecursionAndFunctions/areaAndVolume.fsx
@@ -1,6 +1,9 @@
 let areaOfCircle r = System.Math.PI * r * r
 
 let volumeOfCylinder r h = areaOfCircle r * h
+let areaOfCircleWithCarry r = fun () -> System.Math.PI * r * r
+
+let volumeOfCylinderWithCarry r = fun h -> areaOfCircleWithCarry r () * h
 
 let main () =
     printf "Введите радиус круга: "
@@ -15,4 +18,15 @@ let main () =
     printfn "Площадь круга: %f" area
     printfn "Объем цилиндра: %f" volume
 
+    printf "Введите радиус круга: "
+    let radius = float (System.Console.ReadLine())
+
+    printf "Введите высоту цилиндра: "
+    let height = float (System.Console.ReadLine())
+
+    let area = areaOfCircleWithCarry radius ()
+    let volume = volumeOfCylinderWithCarry radius height
+
+    printfn "Площадь круга: %f" area
+    printfn "Объем цилиндра: %f" volume
 main ()
