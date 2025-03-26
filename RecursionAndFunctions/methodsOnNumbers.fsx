96bcb89255368d0dc0d9db1cbdd9ee488b8f096c
Asya <111695385+asyanix@users.noreply.github.com>
2025-03-26 15:54:42 +0300

    Task 16.2: Find the number of digits of a number less than 3.

--- RecursionAndFunctions/methodsOnNumbers.fsx
+++ RecursionAndFunctions/methodsOnNumbers.fsx
@@ -20,3 +20,30 @@ let sumOfPrimeDiv number =
     sumOfPrimeDiv 1 0
 
 System.Console.WriteLine(sumOfPrimeDiv 39)
+
+// Метод 2. Найти количество цифр числа, меньших 3.
+
+let rec reduce num (func:int->int->int) acc: int  =
+    match num with
+    | 0 -> acc
+    | _ -> reduce (num/10) func (func acc (num%10)) 
+
+let countDigitsGreaterThan3 number =
+    reduce number (fun acc d -> 
+        match d > 3 with
+        | true -> acc + 1
+        | _ -> acc
+    ) 0
+ 
+let productOfDivisorsWithSmallerDigitSum number =
+    let totalSum = reduce number (fun acc d -> acc + d) 0
+    let rec findProduct d acc =
+        match d > number with
+        | true -> acc
+        | false -> 
+            match number % d, reduce d (fun acc x -> acc + x) 0 < totalSum with
+            | 0, true -> findProduct (d + 1) (acc * d)
+            | _, _ -> findProduct (d + 1) acc
+    findProduct 1 1
+
+System.Console.WriteLine(countDigitsGreaterThan3 173829)
