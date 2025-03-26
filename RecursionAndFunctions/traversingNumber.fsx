168ea092c8c5c681018b3725e6c674398c0797ba
Asya <111695385+asyanix@users.noreply.github.com>
2025-03-26 10:46:56 +0300

    Task 8: Test this function on sum, product, minimum, maximum digits of a number operations.

--- RecursionAndFunctions/traversingNumber.fsx
+++ RecursionAndFunctions/traversingNumber.fsx
@@ -2,12 +2,29 @@
 // принимает три аргумента, число, функция (например, сумма, произведение, минимум, максимум)
 // и инициализирующее значение. Функция должна иметь два Int аргумента и возвращать Int.
 
+// Задание 8. Протестировать эту функцию на операциях  сумма, произведение, минимум, максимум 
+// цифр числа. Для тестирования и передачи аргумента использовать лямбда выражения. 
+// Инициализирующее заполнение должно иметь значение по умолчанию.
+
 let rec obhod_chisla n (f: int->int->int) acc =
      match n with
      | 0 -> acc
      | n -> obhod_chisla (n/10) f (f acc (n%10))
 
-let inc_ a b = a+1 in
-System.Console.WriteLine(obhod_chisla 325 inc_ 0)
-let sum_ a b = a+b in
+let count_ a b = a+1
+System.Console.WriteLine(obhod_chisla 412 count_ 0)
+ 
+let sum_ a b = a+b
 System.Console.WriteLine(obhod_chisla 634 sum_ 0)
+ 
+let minDigit a b =
+    match (a, b) with
+    | (a, b) when a <= b -> a
+    | _ -> b
+System.Console.WriteLine(obhod_chisla 865 minDigit 9)
+ 
+let maxDigit a b =
+    match (a, b) with
+    | (a, b) when a >= b -> a
+    | _ -> b
+System.Console.WriteLine(obhod_chisla 348 maxDigit 0)
